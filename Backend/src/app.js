3a46bdeb443d953ebf4f9e92099f92637fe584ea
Pooja <poojadnikam@coep.sveri.ac.in>
2026-07-13 10:14:37 +0530

    backend connected with database

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -3,6 +3,7 @@ import cors from "cors";
 
 import "./Config/db.js";
 
+import member from "./Routes/member.js";
 import membership from "./Routes/membership.js";
 import attendance from "./Routes/attendance.js";
 import profile from "./Routes/profile.js";
@@ -17,6 +18,7 @@ app.get("/", (req, res) => {
   res.send("Gym Management System Backend is Running");
 });
 
+app.use("/api/member",member);
 app.use("/api/membership", membership);
 app.use("/api/attendance", attendance);
 app.use("/api/profile", profile);
